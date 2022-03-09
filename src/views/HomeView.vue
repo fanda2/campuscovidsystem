51117240e26acdd9849c9edc0010ec5846e30a0a
fanda2 <3055093273@qq.com>
2022-03-09 12:29:00 +0800

    重新构建框架

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span>学生信息管理</span>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
